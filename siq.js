38bae05d3fc767cee7340f4f645d7732b4e1d630
Jason Hamner <ke7ori@gmail.com>
2016-03-03 10:19:41 -0700

    init project

---
+++ siq.js
@@ -0,0 +1,122 @@
+/**
+ * Created by brian on 2/23/16.
+ */
+console.log('loading server');
+var fs = require('fs');
+var express = require('express');
+
+//modules below are express middleware
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var _ = require('underscore');
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Bronco@23',
+  database : 'tysql'
+});
+
+connection.connect();
+
+var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
+
+app.use(compression());
+
+app.use(allowCrossDomain);
+
+var db = [
+        { id:1, panelHeading:"Foo", "panelBody":"All your base are belong to us!" },
+        { id:2, "panelHeading":"Bar", "panelBody":"I can haz cheesburger?" },
+        { id:3, "panelHeading":"Baz", "panelBody":"Hooked on phonics worked for me!" }
+];
+
+//REST API calls go here.
+app.get('/api/testdb', function(req, res){
+	connection.query("SELECT cust_id as 'id', cust_name as 'Name', cust_zip as 'Zip' from customers", function(err, rows, fields) {
+		if (err) throw err;
+		//console.log('The solution is: ', rows[0].solution);
+		res.json(rows);
+	});
+});
+
+app.get('/api/entries', function(req, res) {
+    var headings = _.map(db, function(entry){
+        var heading = {};
+        heading.id = entry.id;
+        heading.panelHeading = entry.panelHeading;
+        return heading;
+    });
+    res.json(headings);
+});
+
+// IDEMPOTENT - You can repeat the operation as many times as you want without changing state.
+// Create
+app.post('/api/entries', function(req, res){
+    // Store new entry and return id.
+});
+
+// Read
+app.get('/api/entries/:id', function(req, res){
+    var id = req.params.id;
+    res.json(db[id - 1]);
+});
+
+// Update
+app.put('/api/entries/:id', function(req, res){
+
+});
+
+// Delete
+app.delete('/api/entries/:id', function(req, res){
+
+});
+
+//traditional webserver stuff for serving static files
+var WEB = __dirname + '/web';
+app.use(favicon(WEB + '/favicon.ico'));
+app.use(express.static(WEB));
+app.get('*', function(req, res) {
+	res.header('Access-Control-Allow-Origin', '*');
+    res.status(404).sendFile(WEB + '/404Error.png');
+});
+
+//var config = JSON.parse(fs.readFileSync("/dev/nodejs/resumeServer.json"));
+var port = process.env.port || 3000;
+var server = app.listen(port);
+
+function gracefullShutdown(){
+	console.log('\n starting shut down');
+	server.close(function(){
+		connection.end();
+		console.log('Shut down complete');
+	});
+}
+
+process.on('SIGTERM', function(){
+	gracefullShutdown();
+});
+
+process.on('SIGINT', function(){
+	gracefullShutdown();
+});
+
+
+
+console.log(`Listening on port ${port}`);
+
